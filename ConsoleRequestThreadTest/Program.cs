bd68605837fb0026a59e53e4a4aa1e2bfa79e5ba
Keith Grima <Keith Grima>
2017-07-03 22:16:50 +0200

    added tasks/thread tutorial needed to test something with log4net properties

---
+++ ConsoleRequestThreadTest/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleRequestThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = Task.Run(async () =>
+            {
+                do
+                {
+                    Random r = new Random();
+                    var delay = r.Next(1000, 2000);
+
+                    await Task.Delay(delay);
+
+                    WebClient client = new WebClient();
+                    var guid = Guid.NewGuid().ToString();
+                    var response = client.DownloadString("http://localhost:55863/api/Values/Get1?token=" + guid);
+                    Console.WriteLine("GET1:" + response + " Delay:" + delay);
+                } while (true);
+            });
+
+            var t2 = Task.Run(async () =>
+            {
+                do
+                {
+                    Random r = new Random();
+                    var delay = r.Next(1000, 5000);
+
+                    await Task.Delay(delay);
+
+                    WebClient client = new WebClient();
+                    var response = client.DownloadString("http://localhost:55863/api/Values/Get2");
+                    Console.WriteLine("GET2:" + response + " Delay:" + delay);
+                } while (true);
+            });
+
+            var t3 = Task.Run(async () =>
+            {
+                do
+                {
+                    Random r = new Random();
+                    await Task.Delay(1000);
+                    
+                    Console.WriteLine($"ThreadID for Task3:{Thread.CurrentThread.ManagedThreadId}");
+                } while (true);
+            });
+
+            new Thread(() => {
+                do
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"ThreadID for ThreadLoop:{Thread.CurrentThread.ManagedThreadId}");
+                } while (true);
+            }).Start();
+
+            Task.WaitAll(new Task[] { t1, t2, t3 });
+        }
+    }
+}
---
+++ WebAppThreadTest/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebAppThreadTest.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public async Task<dynamic> Get1(string token)
+        {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+
+            var t = Task.Run(() => SetToken(token));
+            await t;
+
+            var logToken = log4net.ThreadContext.Properties["token"];
+
+            var result = $"ThreadID:{threadID}|Token:{logToken}";
+            return result;
+        }
+
+        public void SetToken(string token)
+        {
+            log4net.ThreadContext.Properties["token"] = token;
+        }
+
+        // GET api/values/5
+        public dynamic Get2()
+        {
+            var token = log4net.ThreadContext.Properties["token"];
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+            var result = $"ThreadID:{threadID}|Token:{token}";
+            return result;
+        }
+
+
+    }
+}
